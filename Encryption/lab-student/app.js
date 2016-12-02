aa6b4f151265fb680244e8ba835d16be2f31e1e5
Jayan Zaman <jayanzaman@gmail.com>
2016-12-01 22:46:03 -0500

    third commit

--- Encryption/lab-student/app.js
+++ Encryption/lab-student/app.js
@@ -12,70 +12,72 @@ app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use("/", express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 app.use(session({
-  secret: 'theTruthIsOutThere51',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
+    secret: 'theTruthIsOutThere51',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false
+    }
 }))
 
-var db = pgp('postgres://bryan@localhost:5432/auth_xfiles');
+var db = pgp('postgres://jayanzaman@localhost:5432/auth_xfiles');
 
-app.get("/", function(req, res){
-  var logged_in;
-  var email;
+app.get("/", function(req, res) {
+    var logged_in;
+    var email;
 
-  if(req.session.user){
-    logged_in = true;
-    email = req.session.user.email;
-  }
+    if (req.session.user) {
+        logged_in = true;
+        email = req.session.user.email;
+    }
 
-  var data = {
-    "logged_in": logged_in,
-    "email": email
-  }
+    var data = {
+        "logged_in": logged_in,
+        "email": email
+    }
 
-  res.render('index', data);
+    res.render('index', data);
 });
 
-app.get("/signup", function(req, res){
-  res.render('signup/index')
+app.get("/signup", function(req, res) {
+    res.render('signup/index')
 });
 
-app.post('/signup', function(req, res){
-  var data = req.body;
+app.post('/signup', function(req, res) {
+    var data = req.body;
 
-  bcrypt.hash(data.password, 10, function(err, hash){
-    db.none(
-      "INSERT INTO users (email, password_digest) VALUES ($1, $2)",
-      [data.email, hash]
-    ).then(function(){
-      res.send('User created!');
-    })
-  });
+    bcrypt.hash(data.password, 10, function(err, hash) {
+        db.none(
+            "INSERT INTO users (email, password_digest) VALUES ($1, $2)", [data.email, hash]
+        ).then(function() {
+            res.send('User created!');
+        })
+    });
 })
 
-app.post('/login', function(req, res){
-  var data = req.body;
+app.post('/login', function(req, res) {
+    var data = req.body;
 
-  db.one(
-    "SELECT * FROM users WHERE email = $1",
-    [data.email]
-  ).catch(function(){
-    res.send('Email/Password not found.')
-  }).then(function(user){
-    bcrypt.compare(data.password, user.password_digest, function(err, cmp){
-      if(cmp){
-        req.session.user = user;
-        res.redirect('/');
-      } else {
+    db.one(
+        "SELECT * FROM users WHERE email = $1", [data.email]
+    ).catch(function() {
         res.send('Email/Password not found.')
-      }
+    }).then(function(user) {
+        bcrypt.compare(data.password, user.password_digest, function(err, cmp) {
+            if (cmp) {
+                req.session.user = user;
+                res.redirect('/');
+            } else {
+                res.send('Email/Password not found.')
+            }
+        });
     });
-  });
 });
 
 
@@ -84,6 +86,6 @@ app.post('/login', function(req, res){
 
 
 
-app.listen(3000, function () {
-  console.log('X-Files Auth App: listening on port 3000!');
+app.listen(3000, function() {
+    console.log('X-Files Auth App: listening on port 3000!');
 });
